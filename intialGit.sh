dda1a4d511bf4277198dea93338600a42a4d2235
Sam Wilk <swilk@beeline.com>
2023-01-10 09:42:50 -0500

    Setting up repo

---
+++ intialGit.sh
@@ -0,0 +1,7 @@
+echo "# pokemon-react" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SamWilk/pokemon-react.git
+git push -u origin main
