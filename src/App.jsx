009563783abe56950a7579537085e77d3b084739
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2023-01-10 14:04:09 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div>Making a site for when I play Pokemon games</div>
-      <div>Testing github actions, take Three</div>
+      <div>Testing github actions, take four</div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
