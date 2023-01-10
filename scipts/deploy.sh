8ff52a76b9507a713d921800aaf00e4f8eaeed28
Sam Wilk <swilk@beeline.com>
2023-01-10 15:11:55 -0500

    testing things with actions

--- scipts/deploy.sh
+++ scipts/deploy.sh
@@ -22,9 +22,6 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/SamWilk/pokemon-react.git main:gh-pages
 
