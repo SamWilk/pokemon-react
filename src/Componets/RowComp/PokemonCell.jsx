cfcce62b3cc424e912b4476519ab772020587546
SamWilk <samwilk18@yahoo.com>
2023-01-11 16:13:06 -0500

    Chaning styles and removing bugs

--- src/Componets/RowComp/PokemonCell.jsx
+++ src/Componets/RowComp/PokemonCell.jsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from "react";
 import "./PokemonCell.css";
 
 const PokemonCell = ({ pokemon }) => {
-  if (pokemon == undefined) {
-    console.log("Nothing to see here!");
-    return <div />;
-  }
+  const [type, setType] = useState([]);
+
+  useEffect(() => {
+    FindType();
+  }, []);
+
+  const FindType = async () => {
+    let tempArray = [];
+    if (pokemon != undefined) {
+      while (pokemon.types.length != 0) {
+        const typeBefore = pokemon.types.pop();
+        const url = typeBefore.type.url;
+        const response = await fetch(url);
+        const body = await response.json();
+        tempArray.push(body.name);
+      }
+      setType(tempArray);
+    }
+  };
+
   return (
     <div id="zoom" className="cell">
-      <div className="subHeader">
-        <div>{pokemon.id}</div>
-        <div>{pokemon.name}</div>
-      </div>
-      <div>
-        <img id="zoom" src={pokemon.sprites.front_default} />
-      </div>
+      {pokemon == undefined ? (
+        <div />
+      ) : (
+        <span>
+          <div className="subHeader">
+            <div>{pokemon.id}</div>
+            <div>{pokemon.name}</div>
+          </div>
+          <div>
+            <img id="zoom" src={pokemon.sprites.front_default} />
+          </div>
+          <div className="typeRow">
+            {type.map((typ, idx) => {
+              return <div key={idx}>{typ}</div>;
+            })}
+          </div>
+        </span>
+      )}
     </div>
   );
 };
