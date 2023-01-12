0998d894fc1304cbfe1a5ecbf877fda007057429
SamWilk <samwilk18@yahoo.com>
2023-01-11 20:53:06 -0500

    Writing down some thoughts

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import PokemonCell from "./Componets/RowComp/PokemonCell.jsx";
 
+/*
+ * Construct a pokemon object to pass around in the application rather than work with requests
+ * all the time.
+ * Call once load times are down and less api calls
+ */
+
 function App() {
   const [pokeapi, setPokeapi] = useState(new Array());
   const [search, setSearch] = useState("");
