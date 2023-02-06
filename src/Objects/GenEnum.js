cff6618e493bfb4c3569929c00368e217f4bab39
SamWilk <samwilk18@yahoo.com>
2023-02-05 19:53:53 -0500

    Small changes

--- src/Objects/GenEnum.js
+++ src/Objects/GenEnum.js
@@ -1,5 +1,6 @@
 export const GenEnum = {
-  Gen1: { limit: 151 },
+  Dev: { limit: 10, offset: 0 },
+  Gen1: { limit: 151, offset: 0 },
   Gen2: { limit: 100, offset: 151 },
   Gen3: { limit: 135, offset: 251 },
 };
