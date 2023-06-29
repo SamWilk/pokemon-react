4c9582dcd9fc93f737e75f6867018b91e7fcc792
Sam Wilk <swilk@beeline.com>
2023-06-29 11:15:44 -0400

    Updating styles of added buttons for generations

--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,26 +1,66 @@
 import { useEffect, useState } from "react";
 import { PokemonList } from "../PokemonList/PokemonList";
+import "./Pokemon.css";
+import { RadioGroup, FormControlLabel } from "react-radio-group";
 
 const Pokemon = () => {
+  const [pokemonList, SetPokemonList] = useState(new Array());
+  const [pokemonGen, SetPokemonGen] = useState(0);
+  let genArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   useEffect(() => {
     GetPokemon();
   }, []);
 
-  const [pokemonList, SetPokemonList] = useState(new Array());
-
   const GetPokemon = async () => {
-    const response = await fetch("http://localhost:3000/pokemon");
-    const pokemonList = await response.json();
-    SetPokemonList(pokemonList);
+    if (pokemonGen == 0) {
+      const response = await fetch("http://localhost:3000/pokemon");
+      const pokemonList = await response.json();
+      SetPokemonList(pokemonList);
+    } else {
+      const response = await fetch("http://localhost:3000/pokemon/gen", {
+        method: "get",
+        headers: { "Content-Type": "application/json" },
+        body: { generation: pokemonGen },
+      });
+      const pokemonList = await response.json();
+      SetPokemonList(pokemonList);
+    }
+  };
+
+  const GenFilter = async (gen) => {
+    SetPokemonGen(gen);
+    console.log(gen);
   };
 
   return (
-    <div>
-      {pokemonList.length >= 1 ? (
-        <PokemonList List={pokemonList} />
-      ) : (
-        <div>Bear with me now...</div>
-      )}
+    <div className='ListHolder'>
+      <div className='SideColumn'>
+        {/* <GenFilter /> */}
+        <h4>Choose the Generation</h4>
+        <div className='GenContainer'>
+          {genArray.map((gen) => {
+            return (
+              <input
+                className='GenButton'
+                key={gen}
+                type='button'
+                value={`Gen ${gen}`}
+                onClick={async () => {
+                  await GenFilter(gen);
+                }}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className='ListHolder'>
+        {pokemonList.length >= 1 ? (
+          <PokemonList List={pokemonList} />
+        ) : (
+          <div>Bear with me now...</div>
+        )}
+      </div>
     </div>
   );
 };
