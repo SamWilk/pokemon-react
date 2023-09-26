80b83c218cfe05991e667efe7e8980fd23c256d4
Sam Wilk <swilk@beeline.com>
2023-09-26 19:26:34 -0400

    Allowing user to update pokemone cells on click

--- src/Components/RowComp/PokemonCell.jsx
+++ src/Components/RowComp/PokemonCell.jsx
@@ -1,16 +1,49 @@
 import { CheckTyping } from "../../logic/CheckTyping.jsx";
 import "./PokemonCell.css";
+import { getMyAPIUrl } from "../../configURL.js";
+import { useCookies } from "react-cookie";
+import React from "react";
 
 const PokemonCell = ({ pokemon }) => {
+  let className = "cell";
+  const APIUrl = getMyAPIUrl();
+  const [cookies] = useCookies(["Bearer"]);
+  const [, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+
+  if (pokemon.Selected === true) {
+    className = "cell-selected";
+  }
+
+  const updatePokemon = async (pokemon) => {
+    try {
+      const response = await fetch(`${APIUrl}/users/pokemon`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.Bearer}`,
+        },
+        body: JSON.stringify({ pokemonid: pokemon.id }),
+      });
+      console.log(response);
+      if (response.status == "202") {
+        pokemon.Selected = false;
+      } else {
+        pokemon.Selected = true;
+      }
+      forceUpdate();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div id="zoom" className="cell" onClick={() => {
-      console.log(pokemon)
-    }}>
+    <div id='zoom' className={className} onClick={() => updatePokemon(pokemon)}>
       {pokemon == undefined ? (
         <div />
       ) : (
         <span>
-          <div className="subHeader">
+          <div className='subHeader'>
             <div>{pokemon.id}</div>
             <div>{pokemon.name}</div>
           </div>
