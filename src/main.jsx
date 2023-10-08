d348bdaeb7767f7ae8e09eebf705fbf0143daae6
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2023-10-08 17:51:28 -0400

    Adding a user profile page

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import { Provider } from "react-redux";
 import store from "./Store/store";
+import UserPage from "./Components/UserPage/User";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/pokemon-react' element={<App />} />
-          <Route path='/pokemon-react/login' element={<Login />} />
+          <Route path="/pokemon-react" element={<App />} />
+          <Route path="/pokemon-react/login" element={<Login />} />
+          <Route path="/pokemon-react/userpage" element={<UserPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
