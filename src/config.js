75fd99af40610f2168ae526ac9d20da3bf7ae36b
SamWilk <samwilk18@yahoo.com>
2024-12-08 19:42:45 -0500

    Updating ports

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 
 const config = {
   production: {
-    SiteUrl: "http://139.144.255.36:4000", // Your production API URL
-    APIUrl: "http://139.144.255.36:4000/api",
+    SiteUrl: "http://139.144.255.36", // Your production API URL
+    APIUrl: "http://139.144.255.36/api",
   },
   development: {
     SiteUrl: "http://localhost", // Your development API URL
