e75a93f6111046ce5579bf112bccc48560c216ea
SamWilk <samwilk18@yahoo.com>
2024-12-08 22:03:01 -0500

    Cleaning up the repo some

--- psql-files/Poke-FlagTabkeCreation.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE
-    IF NOT EXISTS 
-    pokemonflag (
-        pokemonflagID UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-        pokemonID int not null,
-        userID UUID not null,
-        createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
--- psql-files/UserTableCreation.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE
-    IF NOT EXISTS  users (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
