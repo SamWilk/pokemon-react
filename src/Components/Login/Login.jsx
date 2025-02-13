4f957eca9612c4527784ba6fc475f55e0ba8102e
Sam Wilk <sam.wilk@beeline.com>
2025-02-13 15:25:10 -0500

    Updating config

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,28 +7,26 @@ import pokeball from "../../../public/pokeball-outline.png";
 const Login = () => {
   const [loginState, setLoginState] = useState(true);
   return (
-    <div className="App">
-      <div className="Sub-App-Container">
-        <div className="Login-Title">
+    <div className='App'>
+      <div className='Sub-App-Container'>
+        <div className='Login-Title'>
           <h1>Pokemon React Tracker</h1>
-          <img src={pokeball} alt="Pokemon-Ball" height={150} width={150} />
+          <img src={pokeball} alt='Pokemon-Ball' height={150} width={150} />
         </div>
 
-        <div className="Login-screen">
+        <div className='Login-screen'>
           {loginState ? <LoginForm /> : null}
-
           {!loginState ? <SignUpForm /> : null}
           <button
-            className="submit-button"
+            className='submit-button'
             onClick={() => setLoginState(!loginState)}
           >
             {loginState ? "Sign Up Form" : "Login In Form"}
-          </button>
+          </button>{" "}
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Login;
